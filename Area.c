eca4291c6b60ad360f3ae6235ba481d47d03e74d
ImpulsePrime <michukifidel@gmail.com>
2024-09-12 08:51:40 +0300

    Added

---
+++ Area.c
@@ -0,0 +1,13 @@
+//Calculates area of a rectangle
+#include<stdio.h>
+int main(){
+    int l, w, area;
+    printf("Enter length ");
+    scanf("%d", &l);
+    printf("Enter width ");
+    scanf("%d", &w);
+    area= l * w;
+    printf("Area is %d", area);
+    return 0;
+
+}

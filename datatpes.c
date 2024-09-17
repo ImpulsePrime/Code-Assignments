80cf7f918baa03ac83ab4d86616725e7e215aef3
ImpulsePrime <michukifidel@gmail.com>
2024-09-17 09:43:08 +0300

    files

---
+++ datatpes.c
@@ -0,0 +1,18 @@
+//C variables and data types
+//Pre processor directive
+#include <stdio.h> //printf(), scanf()
+int main(){
+    //declararion and initialization
+    char a= 'K';
+    char name[]= "Fidel";
+    int age= 10; //whole numbers
+    float area= 20.035;
+    double salary=20000.50;
+    printf("The character is %c \n",a);
+    printf("The string is %s \n",name);
+    printf("The interger is %d years \n", age);
+    printf("The floating point is %.3f\n",area);
+    printf("The double is %10.2lf", salary);
+    
+    return 0;
+}
---
+++ datatypes promptuser.c
@@ -0,0 +1,33 @@
+//C variables and data types
+//Pre processor directive
+#include <stdio.h> //printf(), scanf()
+int main(){
+    //declararion and initialization
+    char a;
+    char name[]={};
+    int age; 
+    float area;
+    double salary;
+    
+    printf("Enter a character ");
+    scanf("%c",&a);
+    printf("The character is %c \n",a);
+    
+    printf("Enter your name ");
+    scanf("%s",&name);
+    printf("The string is %s \n",name);
+    
+    printf("Enter your age ");
+    scanf("%d",&age);
+    printf("The interger is %d years \n", age);
+    
+    printf("Enter the area ");
+    scanf("%f",&area);
+    printf("The floating point is %.3f\n",area);
+    
+    printf("Enter salary ");
+    scanf("%lf",&salary);
+    printf("The double is %.2lf", salary);
+    
+    return 0;
+}
